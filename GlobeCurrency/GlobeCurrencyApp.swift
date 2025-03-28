714d1f0aa3ac58b40b81ae7d08ecdacc1c8626fa
Ahmed Fareed <ahmed.fareed@mdi.com.eg>
2025-03-28 02:33:48 +0200

    Initial Commit

---
+++ GlobeCurrency/GlobeCurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlobeCurrencyApp.swift
+//  GlobeCurrency
+//
+//  Created by Ahmed Fareed on 28/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct GlobeCurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
